0f3104df54a3cd38d024078c226dc6014d0bbbfb
Kristin Corona <kristincorona@gmail.com>
2015-06-24 20:02:00 -0400

    Defined member routes and a catchall route for static files

--- index.js
+++ index.js
@@ -7,8 +7,27 @@ http.createServer(function (req, res) {
 	// In case client uses lower case characters for methods
 	req.method = req.method.toUpperCase();
 	console.log(req.method + ' ' + req.url);
-	res.end('The current time is ' + Date.now())
 	
+	if (req.method !== 'GET') {
+		res.writeHead(501, {
+			'Content-Type': 'text/plain'
+		});
+		return res.end(req.method + ' is not implemented by this server.');
+	}
+
+	if (_url = /^\/members$/i.exec(req.url)) {
+		// return a list of members
+		res.writeHead(200)
+		return res.end('member list');
+	} else if (_url = /^\/members$/i.exec(req.url)) {
+		// find the member by id defined in route
+		res.writeHead(200);
+		return res.end('a single member');
+	} else {
+		// attempt to send static file
+		res.writeHead(200);
+		res.end('static file maybe')
+	}
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
