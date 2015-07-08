c98dcdbb6714ae4c4e2ca417a2f3c9b1d685e650
Kristin Corona <kristincorona@gmail.com>
2015-07-08 12:50:07 -0400

    Rename members.js to member.js and update model

---
+++ models/member.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var EmployeeSchema = new Schema({
+	id: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	name: {
+		first: {
+			type: String,
+			required: true
+		},
+		last: {
+			type: String,
+			required: true
+		}
+	},
+	businessLine: {
+		type: Schema.Types.ObjectId,
+		ref: 'BusinessLine'
+	},
+	image: {
+		type: String,
+		default: 'images/member.png'
+	},
+	address: {
+		lines: {
+			type: [String]
+		},
+		city: {
+			type: String
+		},
+		zip: {
+			type: Number
+		}
+	}
+});
+
+module.exports = mongoose.model('Member', MemberSchema);
