c821dc5ab1ed284fc64c73697f5e3402bac7be81
Kristin Corona <kristincorona@gmail.com>
2015-07-08 16:05:44 -0400

    Update vendor schema

--- models/vendor.js
+++ models/vendor.js
@@ -4,5 +4,23 @@ var VendorSchema = new Schema({
 	name: {
 		type: String,
 		required: true
+	},
+	contact: {
+		first: {
+			type: String,
+			required: true
+		},
+		last: {
+			type: String, 
+			required: true
+		},
+		phone: {
+			type: String
+		},
+		email: {
+			type: String
+		}
 	}
 });
+
+module.exports = mongoose.model('Vendor', VendorSchema);
